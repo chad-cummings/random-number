d7607209bb33fe8dc437e39cf3c304134ed759a0
Chad Cummings <drinks.sobe@gmail.com>
2016-11-18 01:19:35 -0600

    added endpoint that returns random number

--- src/main/java/com/ctech/HomeController.java
+++ src/main/java/com/ctech/HomeController.java
@@ -1,5 +1,7 @@
 package com.ctech;
 
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,10 @@ public class HomeController {
     @RequestMapping(path = "/")
     public String home() {
         return "Hello REST Microservice World";
+    }
+    
+    @RequestMapping(path = "/random")
+    public String random() {
+    	return String.valueOf(new Random().nextInt(10));
     }
 }
